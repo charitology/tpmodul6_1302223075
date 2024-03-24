bed19d17789449bdfa8751e2d194953b4e7ae869
Dara Sheiba <102475225+charitology@users.noreply.github.com>
2024-03-24 17:56:37 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223075/Program.cs
+++ tpmodul6_1302223075/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo (string title)
+    {
+        this.id = RandomId();
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    private int RandomId()
+    {
+        Random random = new Random();
+        return random.Next(10000, 99999);
+    }
+
+    public void IncreasePlayCount (int count)
+    {
+        playCount=+ count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " +  title);
+        Console.WriteLine("Play Counts: " + playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design by Contract - Dara Sheiba");
+        vid.IncreasePlayCount(200);
+        vid.PrintVideoDetails();
+    }
+}
+
